71c9485066cf9906d1c84bb501213857330c4d37
Tomasz Guzialek <tomasz@guzialek.info>
2016-11-01 21:48:02 +0100

    Webpack prod config.

--- webpack-config/polyfills.ts
+++ webpack-config/polyfills.ts
@@ -3,5 +3,10 @@ import 'core-js/es7/reflect';
 
 require('zone.js/dist/zone');
 
-Error['stackTraceLimit'] = Infinity;
-require('zone.js/dist/long-stack-trace-zone');
+if (process.env.ENV === 'production') {
+  // Production
+} else {
+  // Development
+  Error['stackTraceLimit'] = Infinity;
+  require('zone.js/dist/long-stack-trace-zone');
+}
