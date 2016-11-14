19c5cc438969bf6869b619fee2383a7f264088d0
Tomasz Guzialek <tomasz@guzialek.info>
2016-11-14 22:38:26 +0100

    Adding routes.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { path: '', component: LoginComponent },
+      { path: '**', component: LoginComponent }
+    ])
+  ],
   declarations: [ AppComponent, LoginComponent ],
   bootstrap: [ AppComponent ]
 })
